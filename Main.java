a660579a65ae7ffe8177d924c4fc8ab898b7e5d5
mihocrebeca <115098177+mihocrebeca@users.noreply.github.com>
2022-10-20 19:10:15 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,29 +1,14 @@
-import javax.swing.JOptionPane;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        motto();
-        pretTotal();
-    }
-
-    public static int nrMinuteInchiriere() {
-        System.out.println("Introduceti cate minute doriti sa inchiriati echipamentul:");
-        Scanner sc = new Scanner(System.in);
-        int minute = sc.nextInt();
-        return minute;
-    }
-
-    public static void motto() {
-        JOptionPane.showMessageDialog(null, "Sammy’s makes it fun in the sun");
-    }
-
-    public static void pretTotal() {
-        int minute = nrMinuteInchiriere();
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Deviza este: „Sammy’s makes it fun in the sun”");
+        System.out.println("Introduceti cate minute doriti sa inchiriati un echipament sportiv:");
+        int minute= Integer.parseInt(scanner.next());
         int ore=minute/60;
-        System.out.println("Echipamentul va fi inchiriat pentru "+minute+" minute");
-        System.out.println("Numarul minutelor suplimentare este de:"+(40*ore));
-        System.out.println("Numarul orelor de inchiriere este de:"+ore+" ore" +" \nPretul total de inchiriere cu minutele extra este de:"+(40*ore+1*minute));
-
-
+        System.out.println("Numarul minutelor de inchiriere este de:"+minute);
+        System.out.println("Numarul orelor de inchiriere este de:"+ore);
+        double pretTotal=40*ore+1*minute;
+        System.out.println("Pretul total este :"+pretTotal);
     }
 }
